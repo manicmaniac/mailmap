88c085ae667861c0aae6454c195376d8743017b4
Ryosuke Ito <rito.0305@gmail.com>
2022-09-18 15:39:16 +0900

    Initial commit
    bundle gem --no-exe --no-coc --no-ext --mit --test=minitest --ci=github .

---
+++ lib/mailmap.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "mailmap/version"
+
+module Mailmap
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/mailmap_test.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class MailmapTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Mailmap::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
