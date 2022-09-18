80b8e27c5c8b6de994907654b91476084b162995
Ryosuke Ito <rito.0305@gmail.com>
2022-09-19 01:01:13 +0900

    Remove template test case

--- test/mailmap_test.rb
+++ test/mailmap_test.rb
@@ -6,8 +6,4 @@ class MailmapTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Mailmap::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
