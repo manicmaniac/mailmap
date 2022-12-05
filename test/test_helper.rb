8afe9a64f781ace3d428a120c11943395dafc8b0
Ryosuke Ito <rito.0305@gmail.com>
2022-12-05 21:41:42 +0900

    Take coverage at the start of tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 $LOAD_PATH.unshift(File.expand_path('../lib', __dir__))
-require 'mailmap'
 
+require 'simplecov'
+
+SimpleCov.start do
+  load_profile 'test_frameworks'
+  enable_coverage :branch
+end
+
+require 'mailmap'
 require 'minitest/autorun'
