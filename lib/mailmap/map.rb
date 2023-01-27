8eaafb30d73702a112087804e8c44686e85f1f2a
Ryosuke Ito <rito.0305@gmail.com>
2023-01-27 17:11:28 +0900

    Fix doc comments

--- lib/mailmap/map.rb
+++ lib/mailmap/map.rb
@@ -46,7 +46,9 @@ module Mailmap
     #
     # @param commit_name_or_nil [String, nil] the name in commit or nil
     # @param commit_email [String] the name in commit
-    # @return [Array<String>] if found, a pair of proper name and email or nil
+    # @return [(String, String)] if found, a pair of proper name and email
+    # @return [(String, nil)] if only proper name is found
+    # @return [(nil, String)] if only proper email is found
     # @return [nil] if not found
     def lookup(commit_name_or_nil, commit_email)
       commit_name = commit_name_or_nil&.downcase
@@ -55,12 +57,13 @@ module Mailmap
       hash[commit_name] || hash[nil]
     end
 
-    # Like `git-check-mailmap` command, look up the person's canonical name and email address.
+    # Like +git-check-mailmap+ command, look up the person's canonical name and email address.
     # If found, return them; otherwise return the input as-is.
     #
     # @param commit_name_or_nil [String, nil] the name in commit or nil
     # @param commit_email [String] the email in commit
-    # @return [Array<String>] a pair of proper name and email
+    # @return [(String, String)] a pair of proper name and email
+    # @return [(nil, String)] if proper name is not found and +commit_name+ is not provided
     def resolve(commit_name_or_nil, commit_email)
       proper_name, proper_email = lookup(commit_name_or_nil, commit_email)
       proper_name ||= commit_name_or_nil
