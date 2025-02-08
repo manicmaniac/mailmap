9f1f5b8b23a2a51c2f02074ce10c3f04584a2342
Ryosuke Ito <rito.0305@gmail.com>
2025-02-08 17:52:17 +0900

    Generate test cases statically

--- Rakefile
+++ Rakefile
@@ -2,6 +2,7 @@
 
 require 'bundler/gem_tasks'
 require 'rake/testtask'
+require 'rubocop'
 require 'rubocop/rake_task'
 
 Rake::TestTask.new(:test) do |t|
@@ -18,3 +19,16 @@ end
 RuboCop::RakeTask.new
 
 task default: %i[steep test rubocop]
+
+namespace :test do
+  desc 'Generate test cases'
+  task generate: ['test/exe/check_mailmap_compatibility_test.rb']
+end
+
+file 'test/exe/check_mailmap_compatibility_test.rb' => 'test/exe/check_mailmap_compatibility_test.erb' do |task|
+  require 'erb'
+
+  erb = ERB.new(File.read(task.source), trim_mode: '-')
+  File.write(task.name, erb.result)
+  exit(RuboCop::CLI.new.run(['--auto-correct-all', task.name]))
+end
--- test/exe/check_mailmap_test.rb
+++ test/exe/check_mailmap_test.rb
@@ -6,18 +6,6 @@ require 'tempfile'
 require 'test_helper'
 
 class CheckMailmapTest < Minitest::Test
-  parallelize_me!
-
-  def run(*args, **kwargs, &block)
-    @git_dir = Dir.mktmpdir
-    Minitest.after_run do
-      FileUtils.remove_dir(@git_dir)
-      @git_dir = nil
-    end
-    git_init(@git_dir)
-    super
-  end
-
   def test_executable
     assert(File.executable?(EXECUTABLE_PATH))
   end
@@ -48,65 +36,21 @@ class CheckMailmapTest < Minitest::Test
     assert_predicate(status, :success?)
   end
 
-  mailmap_patterns = {
-    name_email: "Proper Name <commit@email.xx>\n",
-    email_email: "<proper@email.xx> <commit@email.xx>\n",
-    name_email_email: "Proper Name <proper@email.xx> <commit@email.xx>\n",
-    name_email_name_email: "Proper Name <proper@email.xx> Commit Name <commit@email.xx>\n",
-    email_name_email: "<proper@email.xx> Commit Name <commit@email.xx>\n",
-    comment: "# Comment\n",
-    trailing_comment: "Proper Name <commit@email.xx> # Comment\n",
-    invalid_name: "Commit Name\n",
-    invalid_name_name: "Proper Name Commit Name\n",
-    invalid_email: "<commit@email.xx>\n"
-  }
-
-  contact_patterns = {
-    email: '<commit@email.xx>',
-    name_email: 'Commit Name <commit@email.xx>',
-    wrong_email: '<wrong@email.xx>',
-    invalid_name: 'Commit Name'
-  }
-
-  mailmap_patterns.each do |mailmap_key, mailmap_value|
-    contact_patterns.each do |contact_key, contact_value|
-      define_method("test_compatibility_on_#{mailmap_key}_with_#{contact_key}") do
-        Tempfile.create('mailmap', @git_dir) do |mailmap|
-          mailmap.write(mailmap_value)
-          mailmap.rewind
-
-          expected = git_check_mailmap(contact_value, mailmap_path: mailmap.path)
-          actual = check_mailmap(contact_value, mailmap_path: mailmap.path)
-
-          assert_equal(expected[0], actual[0]) # stdout
-          assert_equal(expected[1], actual[1]) # stderr
-          assert_equal(expected[2].exitstatus, actual[2].exitstatus) # status
-        end
-      end
-    end
-  end
-
   private
 
   EXECUTABLE_PATH = File.expand_path('../../exe/check-mailmap', __dir__)
   private_constant :EXECUTABLE_PATH
 
-  SIMPLECOV_SPAWN_PATH = File.expand_path('../simplecov_spawn.rb', __dir__)
-  private_constant :SIMPLECOV_SPAWN_PATH
-
-  def git_init(dir)
-    git_dir = File.join(dir, '.git')
-    FileUtils.makedirs([File.join(git_dir, 'objects'), File.join(git_dir, 'refs')])
-    File.write(File.join(git_dir, 'HEAD'), 'ref: refs/heads/main')
-  end
-
   def check_mailmap(*args, mailmap_path: '', stdin_data: nil)
-    Open3.capture3(RbConfig.ruby, '-r', SIMPLECOV_SPAWN_PATH, EXECUTABLE_PATH, '-f', mailmap_path, *args,
-                   stdin_data: stdin_data)
-  end
-
-  def git_check_mailmap(*args, mailmap_path: '', stdin_data: nil)
-    Open3.capture3('git', '-C', @git_dir, '-c', "mailmap.file=#{mailmap_path}", 'check-mailmap', *args,
-                   stdin_data: stdin_data)
+    Open3.capture3(
+      RbConfig.ruby,
+      '-r',
+      File.expand_path('../simplecov_spawn.rb', __dir__),
+      EXECUTABLE_PATH,
+      '-f',
+      mailmap_path,
+      *args,
+      stdin_data: stdin_data
+    )
   end
 end
